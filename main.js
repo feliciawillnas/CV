297d84aa6b9ef51b2a42adb074f2918db457d8a7
felicia <feliciawillnas@gmail.com>
2022-11-29 15:28:27 +0100

    javascript active tab function, active class css

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 window.addEventListener("DOMContentLoaded", main); 
 
 function main() {
+    activeTab();
+}
+
+function activeTab() {
+    let navItems = document.querySelectorAll(".nav-li");
+    for (let i = 0; i < navItems.length; i++) {
+       navItems[i].addEventListener("click", function(event) {
+          const activeNavItem = document.querySelector(".nav-li.active");  
+          activeNavItem?.classList.remove("active"); 
+          event.currentTarget.classList.add("active");
+       });
+    }
 
 }
+
+//? fungerar som en if sats OM detta är sant gör detta 
