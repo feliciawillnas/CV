e9c0cd6317ba10872b4e9228a8cd6c782d959a65
felicia <feliciawillnas@gmail.com>
2022-12-02 17:29:19 +0100

    readme file

---
+++ README.md
@@ -0,0 +1,7 @@
+# CV
+
+## Information 
+* Vi har fått i uppgift att skapa ett interaktivt digitalt CV som skall presentera mig och mina kunskaper. 
+
+## Demo länk: 
+* https://feliciawillnas.github.io/CV/
--- main.js
+++ main.js
@@ -4,6 +4,10 @@ function main() {
     activeTab();
 }
 
+/**
+ * Skapar en click event, då användaren klickar i nav-baren skall active-klassen läggas på den klickade länken.
+ * For-loopen går igenom länkarna för att lägga till "active"-klassen samt ta bort den.
+ */
 function activeTab() {
     let navItems = document.querySelectorAll(".nav-li");
     for (let i = 0; i < navItems.length; i++) {
@@ -13,7 +17,4 @@ function activeTab() {
           event.currentTarget.classList.add("active");
        });
     }
-
 }
-
-//? fungerar som en if sats OM detta är sant gör detta 
